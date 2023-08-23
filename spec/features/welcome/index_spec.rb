34125cfbee57da3fe5f047fd07da496393935667
Leo Banos Garcia <bsleo625@gmail.com>
2023-08-23 17:32:05 -0600

    Adds links for sign in and sign up

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,12 +4,22 @@ RSpec.describe WelcomeController, type: :feature do
   describe 'visiting welcome page' do
     it 'displays Recipe Keeper at the top center of the page' do
       visit '/'
-      save_and_open_page
+      
       expect(page).to have_content("Recipe Keeper")
     end
 
-    it 'shows a link to sign up in the top right corner of page'
+    it 'shows a link to sign up in the top right corner of page' do
+      visit '/'
 
-    it 'shows a link to sign in in the top right corner of page'
+      expect(page).to have_content("Sign Up")
+      expect(page).to have_link("Sign Up", :href=>"/sign_up")
+    end
+
+    it 'shows a link to sign in in the top right corner of page' do
+      visit '/'
+      
+      expect(page).to have_content("Sign In")
+      expect(page).to have_link("Sign In", :href=>"/sign_in")
+    end
   end
 end
