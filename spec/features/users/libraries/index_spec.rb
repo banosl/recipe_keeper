4231378bf355b43372950721c84d0c875d5edf22
Leo Banos Garcia <bsleo625@gmail.com>
2023-08-25 17:21:53 -0600

    Adds libraries controller

--- spec/features/users/libraries/index_spec.rb
+++ spec/features/users/libraries/index_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe 'library index' do
   before :each do
     @user = User.create({first_name: "Leo", last_name: "Banos Garcia", email: "leo@leo.com"})
-    # @library = @user.library
+    @library = @user.create_library
   end
 
   describe 'visiting a users library' do
     it 'shows a users library page with names library at the top' do
-      binding.pry
+      # binding.pry
       visit user_libraries_path(@user.id)
+
+      expect(page).to have_content("Leo's Library")
     end
   end
 end
