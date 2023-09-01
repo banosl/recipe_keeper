50b9acd1438645aa4c0fe21bd048213a1ad79925
Leo Banos Garcia <bsleo625@gmail.com>
2023-09-01 12:05:21 -0600

    Adds sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
   def create
     binding.pry
+    current_user = User.find_or_create_user(user_params)
+  end
+  
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+  def user_params
+    if !google_user_params[:google_id].nil?
+      return google_user_params
+    end
+  end
+
+  def google_user_params
+    {
+      first_name: auth_hash[:info][:first_name],
+      last_name: auth_hash[:info][:last_name],
+      email: auth_hash[:info][:email],
+      google_id: auth_hash[:uid],
+      google_token: auth_hash[:credentials][:token],
+      google_picture: auth_hash[:info][:image]
+    }
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-  redirect_uri: "https://localhost:3000/auth/google_oauth2/callback"
+  redirect_uri: "http://localhost:3000/auth/google_oauth2/callback"
 end
 OmniAuth.config.allowed_request_methods = %i[post]
