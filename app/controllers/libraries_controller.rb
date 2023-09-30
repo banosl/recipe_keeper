d4f9fe4373b5f69e747b16a4ce76674ef23f30e1
Leo Banos Garcia <bsleo625@gmail.com>
2023-09-29 18:22:24 -0600

    Adds cookbooks to library page

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,6 @@
 class LibrariesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @cookbooks = @user.library.cookbooks
   end
 end
--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -15,9 +15,24 @@ RSpec.describe 'library index' do
 
     it 'shows a button to add a cookbook' do
       visit user_libraries_path(@user.id)
-      save_and_open_page
 
       expect(page).to have_link("Add a cookbook to my library", href: new_user_library_cookbook_path(@user.id, @library.id))
+    end
+
+    it 'shows books in my library' do
+      book1 = create(:cookbook, library: @library)
+      book2 = create(:cookbook, library: @library)
+      book3 = create(:cookbook, library: @library)
+      book4 = create(:cookbook, library: @library)
+      book5 = create(:cookbook, library: @library)
+
+      visit user_libraries_path(@user.id)
+
+      expect(page).to have_content("#{book1.title} by #{book1.author}")
+      expect(page).to have_content("#{book2.title} by #{book2.author}")
+      expect(page).to have_content("#{book3.title} by #{book3.author}")
+      expect(page).to have_content("#{book4.title} by #{book4.author}")
+      expect(page).to have_content("#{book5.title} by #{book5.author}")
     end
   end
 end
