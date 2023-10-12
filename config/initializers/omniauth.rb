b31005cbd4cb91e7e8dd4964b1d55b8aee91124e
Leo Banos Garcia <bsleo625@gmail.com>
2023-10-12 12:04:29 -0600

    Change back the redirecturi

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-  redirect_uri: "https://localhost:3000/auth/google_oauth2/callback"
-  # redirect_uri: "https://recipe-keeper-2jsm.onrender.com/auth/google_oauth2/callback"
+  # redirect_uri: "https://localhost:3000/auth/google_oauth2/callback"
+  redirect_uri: "https://recipe-keeper-2jsm.onrender.com/auth/google_oauth2/callback"
 end
 OmniAuth.config.allowed_request_methods = %i[post]
