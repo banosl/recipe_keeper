334e7d7c35e59832a08bc2568197f8def6702b9e
Leo Banos Garcia <bsleo625@gmail.com>
2023-10-31 12:49:17 -0600

    adds ignore localhost for vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.ignore_localhost = true
 end
 
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
