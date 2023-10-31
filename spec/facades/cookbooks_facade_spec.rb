332b62901e356248d51f7804cab7ff17ada19650
Leo Banos Garcia <bsleo625@gmail.com>
2023-10-31 13:35:57 -0600

    Starts test for cookbooks facade

--- spec/facades/cookbooks_facade_spec.rb
+++ spec/facades/cookbooks_facade_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-# RSpec.describe CookbooksFacade do
-  
-# end
+RSpec.describe CookbooksFacade do
+  describe "#cookbook_matches", :vcr do
+    it "returns the top 5 results from just the author" do
+      author = "inauthor:eng+tie+ang"
+      results = CookbooksFacade.cookbook_matches(author)
+
+      expect(results).to be_a(Array)
+      expect(results.count).to be <= 5
+      
+      results.each do |cookbook|
+        expect(cookbook.authors).to contain("Eng Tie Ang")
+      end
+    end
+
+    it "returns the top 5 results from just the title" do
+      title = "intitle:delightful+thai+cooking"
+      results = CookbooksFacade.cookbook_matches(title)
+
+      expect(results).to be_a(Array)
+      expect(results.count).to be <= 5
+      
+      results.each do |cookbook|
+        expect(cookbook.title).to contain("Delightful Thai Cooking")
+      end
+    end
+
+    it "returns the top 5 results from just the isbn" do
+      isbn = "isbn:0962781045"
+      results = CookbooksFacade.cookbook_matches(isbn)
+
+      expect(results).to be_a(Array)
+      expect(results.count).to be <= 5
+      
+      results.each do |cookbook|
+        expect(cookbook.isbn).to eq("0962781045")
+      end
+    end
+    
+    it "returns the top 5 results from just the title and author" do
+      search_info = "inauthor:eng+tie+ang+intitle:delightful+thai+cooking"
+      results = CookbooksFacade.cookbook_matches(search_info)
+
+      expect(results).to be_a(Array)
+      expect(results.count).to be <= 5
+      
+      results.each do |cookbook|
+        expect(cookbook.title).to contain("Delightful Thai Cooking")
+        expect(cookbook.authors).to contain("Eng Tie Ang")
+      end
+    end
+  end
+end
