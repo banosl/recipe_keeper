78ae297e27d25e7d3d36c9054b2761ca62f8e15f
Leo Banos Garcia <bsleo625@gmail.com>
2023-10-31 18:05:00 -0600

    Adds isbn methods

--- app/poros/cookbook_match.rb
+++ app/poros/cookbook_match.rb
@@ -6,6 +6,7 @@ class CookbookMatch
               :description,
               :image_link,
               :language
+
   def initialize(cookbook_search_result)
     @google_id = cookbook_search_result[:id]
     @title = cookbook_search_result[:volumeInfo][:title]
@@ -17,5 +18,19 @@ class CookbookMatch
     @language = cookbook_search_result[:volumeInfo][:language]
   end
 
-  #write a method for isbn_10 and isbn_13 respectively
+  def isbn_10
+    find_isbn("ISBN_10")
+  end
+
+  def isbn_13
+    find_isbn("ISBN_13")
+  end
+
+  def find_isbn(type)
+    @isbn.find do |h|
+      if h[:type] == type
+       return h[:identifier]
+      end
+    end
+  end
 end
--- spec/poros/cookbook_match_spec.rb
+++ spec/poros/cookbook_match_spec.rb
@@ -42,20 +42,23 @@ RSpec.describe CookbookMatch do
       expect(@cookbook_match.authors).to eq(["Eng Tie Ang"])
       expect(@cookbook_match.published_date).to eq("1990")
       expect(@cookbook_match.description).to eq("Thai food is becoming increasingly popular, and this great new cookbook tellshow to prepare more than 100 delicious Thai dishes, most of which can be madein less than an hour. Includes detailed instructions, line drawings and colorphotographs.",)
-      expect(@cookbook_match.isbn_10).to eq("0962781045")
-      expect(@cookbook_match.isbn_13).to eq("9780962781049")
       expect(@cookbook_match.image_link).to eq("http://books.google.com/books/content?id=qsEOAAAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api")
       expect(@cookbook_match.language).to eq("en")
       expect(@cookbook_match.google_id).to eq("qsEOAAAACAAJ")
-  end
-
+    end
+    
   describe "isbn methods" do
     it "#isbn_10 can return the isbn number for that type" do
-
+      expect(@cookbook_match.isbn_10).to eq("0962781045")
+    end
+  
+    it "#isbn_13 can return the isbn number for that type" do
+      expect(@cookbook_match.isbn_13).to eq("9780962781049")
     end
 
-    it "#isbn_13 can return the isbn number for that type" do
-
+    it "#find_isbn just returns the needed isbn depending on which is requested" do
+      expect(@cookbook_match.find_isbn("ISBN_10")).to eq("0962781045")
+      expect(@cookbook_match.find_isbn("ISBN_13")).to eq("9780962781049")
     end
   end
 end
