1f5cac600f40cbf7d8000b5f854d39a35761b53a
Leo Banos Garcia <bsleo625@gmail.com>
2023-11-01 11:53:51 -0600

    Fixes for when there is not image link in the api result

--- app/poros/cookbook_match.rb
+++ app/poros/cookbook_match.rb
@@ -14,7 +14,8 @@ class CookbookMatch
     @published_date = cookbook_search_result[:volumeInfo][:publishedDate]
     @description = cookbook_search_result[:volumeInfo][:description]
     @isbn = cookbook_search_result[:volumeInfo][:industryIdentifiers]
-    @image_link = cookbook_search_result[:volumeInfo][:imageLinks][:thumbnail]
+    cookbook_search_result[:volumeInfo][:imageLinks].nil? ? @image_link = nil :  @image_link = cookbook_search_result[:volumeInfo][:imageLinks][:thumbnail]
+    
     @language = cookbook_search_result[:volumeInfo][:language]
   end
 
