704f1f1fb9964eb726b3e7c40589f96d22ae272b
Leo Banos Garcia <bsleo625@gmail.com>
2023-11-22 09:50:54 -0700

    Adds testing for cookbook info

--- spec/features/cookbooks/show_spec.rb
+++ spec/features/cookbooks/show_spec.rb
@@ -2,15 +2,29 @@ require "rails_helper"
 
 RSpec.describe "Cookbook Show Page" do
   before :each do
-    user = create(:user, :google)
-    user.create_library
+    @user = create(:user, :google)
+    @user.create_library
     @cookbook = create(:cookbook, library: user.library)
     @chapter = create(:chapter, cookbook: @cookbook)
     @recipes = create_list(:recipe, 20, :breakfast, :salad, :dairy, chapter: @chapter)
   end
   describe "Cookbook info" do
     it "Displays the cookbook's title, subtitle, author(s), publisher, isbn, published date, image, language, description, date added to library, number of recipes" do
-      
+      visit user_library_cookbook_path(@user.id, @user.library.id, @cookbook.id)
+
+      within "#cookbook_details_#{@cookbook.id}" do
+        expect(page).to have_content(@cookbook.title)
+        expect(page).to have_content(@cookbook.subtitle)
+        expect(page).to have_content("Author(s): {@cookbook.authors.to_sentence}")
+        expect(page).to have_content("Published by: #{@cookbook.publisher}")
+        expect(page).to have_content(@cookbook.isbn)
+        expect(page).to have_content("Published: #{@cookbook.published_date}")
+        expect(page).to have_content(@cookbook.image)
+        expect(page).to have_content("Language: #{@cookbook.language}")
+        expect(page).to have_content(@cookbook.description)
+        expect(page).to have_content("Added on: #{@cookbook.created_at}")
+        expect(page).to have_content("Number of recipes added: #{@cookbook.recipes_count}")
+      end
     end
 
     it "If a book doesn't have a subtitle or description the page doesn't even say 'subtitle' or 'description'"
