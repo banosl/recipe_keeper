4baa5b8de371ba14197fa3ef1e1ed4e05b15da19
Leo Banos Garcia <bsleo625@gmail.com>
2023-12-04 13:44:33 -0700

    Adds testing for Identifiers portion of cookbook show

--- spec/features/cookbooks/show_spec.rb
+++ spec/features/cookbooks/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Cookbook Show Page" do
   before :each do
     @user = create(:user, :google)
     @user.create_library
-    @cookbook = create(:cookbook, library: @user.library)
+    @cookbook = create(:cookbook, library: @user.library, isbn: {"ISBN-13": Faker::Barcode.isbn})
     @chapter = create(:chapter, cookbook: @cookbook)
     @recipes = create_list(:recipe, 20, :breakfast, :salad, :dairy, chapter: @chapter)
   end
@@ -22,7 +22,7 @@ RSpec.describe "Cookbook Show Page" do
         expect(page).to have_content(@cookbook.subtitle)
         expect(page).to have_content("Author(s): #{@cookbook.authors.to_sentence}")
         expect(page).to have_content("Published by: #{@cookbook.publisher}, #{@cookbook.published_year}")
-        expect(page).to have_content(@cookbook.isbn)
+        expect(page).to have_content("Identifiers: ISBN-13: #{@cookbook.isbn["ISBN-13"]}")
         expect(page).to have_content("Language: #{@cookbook.language}")
         expect(page).to have_content(@cookbook.description)
         expect(page).to have_content("Added on: #{@cookbook.created_at.strftime("%B %d, %Y")}")
@@ -31,7 +31,7 @@ RSpec.describe "Cookbook Show Page" do
     end
 
     it "If a book doesn't have an image, it displays a default 'no image' image" do
-      cookbook_no_image = create(:cookbook, library: @user.library, image_link: nil)
+      cookbook_no_image = create(:cookbook, library: @user.library, image_link: nil, isbn: {"ISBN-13": Faker::Barcode.isbn})
       visit user_library_cookbook_path(@user.id, @user.library.id, cookbook_no_image.id)
       
       within "#cookbook_image_#{cookbook_no_image.id}" do
@@ -40,11 +40,21 @@ RSpec.describe "Cookbook Show Page" do
     end
 
     it "If published date is nil then 'Published by' is followed by 'Year unknown'" do
-      cookbook_nil_published_year = create(:cookbook, library: @user.library, published_date: nil)
+      cookbook_nil_published_year = create(:cookbook, library: @user.library, published_date: nil, isbn: {"ISBN-13": Faker::Barcode.isbn})
       visit user_library_cookbook_path(@user.id, @user.library.id, cookbook_nil_published_year.id)
 
       within("#cookbook_details_#{cookbook_nil_published_year.id}") do
         expect(page).to have_content("Published by: #{cookbook_nil_published_year.publisher}, Year unknown")
+      end
+    end
+
+    it "If isbn is nil then 'Identifiers' is followed by 'Unknown'" do
+      cookbook_nil_isbn = create(:cookbook, library: @user.library)
+
+      visit user_library_cookbook_path(@user.id, @user.library.id, cookbook_nil_isbn.id)
+
+      within ("#cookbook_details_#{cookbook_nil_isbn.id}") do
+        expect(page).to have_content("Identifiers: Unknown")
       end
     end
   end
